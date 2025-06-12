a9bb82deae729b5fcf52b65477c347314e22eb22
Tristesse02 <mtvu@umass.edu>
2025-06-12 19:56:31 -0400

    Word fading effect in initialization of page

--- components/SocialMusicPortfolio/DescriptionPanel.tsx
+++ components/SocialMusicPortfolio/DescriptionPanel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ContentItem } from "@/types";
 import styles from "../../styles/SocialMusicPortfolio/DescriptionPanel.module.css";
 
@@ -7,19 +7,58 @@ interface Props {
 }
 
 export default function DescriptionPanel({ content }: Props) {
+  const wordsArray = content.description.split(" ");
+  console.log(wordsArray);
+  const [visible, setVisible] = useState<boolean[]>(
+    Array(wordsArray.length).fill(false)
+  );
+
+  useEffect(() => {
+    wordsArray.forEach((_, i) => {
+      setTimeout(() => {
+        setVisible((prev) => {
+          const updated = [...prev];
+          updated[i] = true;
+          return updated;
+        });
+      }, i * 100); // 100ms delay per word
+    });
+  }, [content.description]);
+
   return (
     <div className={styles.panelWrapper}>
       <div className={styles.descriptionBox}>
         <h3 className={styles.descriptionHeading}>Lyrics</h3>
-        <p className={styles.descriptionText}>
-          {content.description.split("\n").map((line, index) => (
-            <span key={index}>
-              {line}
-              <br />
-              <br />
-            </span>
-          ))}
-        </p>
+        <div className={`${styles.descriptionText} ${styles.fadeInParagraph}`}>
+          {wordsArray.map((word, i) => {
+            const endsWithNewLine = word.endsWith("\n");
+            const cleanedWord = word.replace(/\n/g, ""); // fully remove all newline characters
+
+            return endsWithNewLine ? (
+              <div key={i} style={{ display: "inline" }}>
+                <span
+                  className={`${styles.word} ${
+                    visible[i] ? styles.wordVisible : ""
+                  }`}
+                >
+                  {cleanedWord}&nbsp;
+                </span>
+                <br />
+                <br />
+              </div>
+            ) : (
+              <span key={i} className={styles.wordWrapper}>
+                <span
+                  className={`${styles.word} ${
+                    visible[i] ? styles.wordVisible : ""
+                  }`}
+                >
+                  {cleanedWord}&nbsp;
+                </span>
+              </span>
+            );
+          })}
+        </div>
       </div>
       <div className={styles.statsBox}>
         <div className={styles.statsGrid}>
