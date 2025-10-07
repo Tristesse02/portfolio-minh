5c4bba85235bc003368826c34a8fcc699ed8ff99
Tristesse02 <mtvu@umass.edu>
2025-10-07 04:33:27 -0400

    Fix the slider issue!

--- components/ui/Slider.tsx
+++ components/ui/Slider.tsx
@@ -2,7 +2,6 @@
 
 import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
-
 import { cn } from "@/lib/utils";
 
 const Slider = React.forwardRef<
@@ -12,15 +11,22 @@ const Slider = React.forwardRef<
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
+      // Root MUST have height context and align center
       "relative flex w-full touch-none select-none items-center",
       className
     )}
     {...props}
   >
-    <SliderPrimitive.Track className="">
-      <SliderPrimitive.Range className="absolute h-full bg-purple-300" />
+    {/* Track MUST have explicit height and be relative so Range can anchor */}
+    <SliderPrimitive.Track className="relative h-1 w-full rounded-full bg-white/30">
+      <SliderPrimitive.Range className="absolute h-full rounded-full bg-white" />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="" />
+
+    {/* Thumb MUST have size to be visible & focusable */}
+    <SliderPrimitive.Thumb
+      className="block h-3 w-3 rounded-full bg-white shadow outline-none
+                 focus-visible:ring focus-visible:ring-white/70"
+    />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = SliderPrimitive.Root.displayName;
